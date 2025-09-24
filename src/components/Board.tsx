801e813c17b044e479e9dc44fc049df6b85d1fa0
davidzhengc <zhengdavid06@gmail.com>
2025-09-24 17:12:40 +0200

    Add click handling

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,19 +1,44 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 interface SquareProps {
   value: string | null;
+  onSquareClick?: () => void;
 }
 
-const Square = ({ value }: SquareProps) => {
-  return <button className="square">{value}</button>;
+const Square = ({ value, onSquareClick }: SquareProps) => {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 };
 
 const Board = () => {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = useCallback(
+    (i: number) => {
+      const nextSquares = squares.slice();
+      if (xIsNext) {
+        nextSquares[i] = "X";
+      } else {
+        nextSquares[i] = "O";
+      }
+      setSquares(nextSquares);
+      setXIsNext(!xIsNext);
+    },
+    [squares]
+  );
+
   return (
     <div className="board">
       {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((index) => (
-        <Square value={squares[index]} />
+        <Square
+          key={index}
+          value={squares[index]}
+          onSquareClick={() => handleClick(index)}
+        />
       ))}
     </div>
   );
